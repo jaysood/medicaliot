1425f990dd016e046daa35916159a31668771c0c
Jay Rauniar Sood <jay.sood98@gmail.com>
2020-03-15 19:29:16 +0000

    Major improvements, json output file and non hard coded generator

--- datagen.py
+++ datagen.py
@@ -1,36 +1,58 @@
-import math
-import random
-import sys
-import os
+import math, random, sys, os, json
 
-data = [0,0,0,0,0]
-monitor = ["blood","glucose","CO2","mmHg","yeet"]
-
-def initialise():
+def initialise_tool():
     print("Warning this tool removes all previously generated data! Please save in a different directory if required!")
     decision = input("Proceed? (Y/N): ")
     if ((decision == 'Y') or (decision == 'y')):
-        if(os.path.isfile('generated.txt')):
+        if(os.path.isfile('generated.json')):
             print("Data deleted.")
-            os.remove("generated.txt")
+            os.remove("generated.json")
         else:
             print("No data found, continuing!")
     else:
         print("Goodbye!")
         sys.exit()
 
-def randomise():
-    for i in range(len(data)):
-        data[i] = random.randint(1,10)
-    data[4] = monitor[random.randint(0,len(monitor)-1)]
 
-def fileIO(generated):
-    dataFile = open("generated.txt", "a")
-    dataFile.write(generated + "\n")
-    dataFile.close()
+def initialise_json():
+    with open('IoMT.json') as f:
+        device_dict = json.load(f)
+        return [device_dict, len(device_dict.keys())]
 
-def main() :
-    initialise()
+
+def generate(dict, noKeys):
+    # Picks a random device from the JSON data
+    # Generates pseudorandom attribues dependant on device
+
+    # Choose random Device Type:
+    random_index = random.randint(0,noKeys - 1)
+    random_device = dict[list(dict.keys())[random_index]] #random.choice?()
+    random_dict = {}
+
+    # Generate random attributes:
+    for key in random_device["Attributes"]:
+        random_dict[key] = randomValueFromDict(random_device["Attributes"][key])
+
+    with open('generated.json', 'w+') as filew:
+        json.dump(random_dict, filew, indent = 2)
+        open into a dictionary containing a list of dictionarys
+
+def randomValueFromDict(generator):
+    #not complete, weird sensor behaviour, try catch needed
+    outlist = ['yeet']
+    if len(generator) == 1:
+        return generator
+    elif isinstance(generator[0],int):
+        return random.randint(generator[0],generator[1])
+    elif isinstance(generator[0],str) and len(generator) > 1:
+        for i in range(0,random.randint(0,len(generator))):
+            outlist.append(generator[i])
+        return outlist
+
+
+def main():
+    initialise_tool()
+    IoMT_Data, lenKeys = initialise_json()
     try:
         noRecords = int(input("Enter number of entries to generate:"))
     except ValueError:
@@ -39,8 +61,7 @@ def main() :
     print("Generating " + str(noRecords) + " entries...")
 
     for i in range(noRecords):
-        randomise()
-        fileIO(str(data))
+        generate(IoMT_Data, lenKeys)
 
     print("Data generated!")
 
