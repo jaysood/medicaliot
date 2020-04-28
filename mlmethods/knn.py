34d92cd9826dd90dda3ff749f36f48206ac41d43
Jay Rauniar Sood <jay.sood98@gmail.com>
2020-04-28 04:00:10 +0100

    Updated IoMT.json data values for accurate ML training.

--- mlmethods/knn.py
+++ mlmethods/knn.py
@@ -1,6 +1,4 @@
-import math
+from sklearn.neighbours import NearestNeighbours
+import numpy as np
 
-def euclidDist(p1,p2):
-    return (math.sqrt((math.pow(abs(p2[0]-p1[0]),2) + math.pow(abs(p2[1]-p1[1]),2))))
-
-print(euclidDist([1,1], [2,2]))
+ 
