685bdff2cbaa53caa1590e8006043c24fc28aa4a
Jay Rauniar Sood <jay.sood98@gmail.com>
2020-04-30 01:18:01 +0100

    Completed conversion to numpy tool.

--- conversion/convert.py
+++ conversion/convert.py
@@ -5,12 +5,19 @@ import numpy as np
 generatedDataFile = Path(r'/Users/jayrsood/Documents/3rd Year/medicaliot/medicaliot/datagen/generated.json')
 iomtDataFile = Path(r'/Users/jayrsood/Documents/3rd Year/medicaliot/medicaliot/datagen/IoMT.json')
 
+def formatStringList(value_list, hashtable):
+    value_key_list = []
+    for string in range(0, len(value_list)):
+        value_key = getKeyFromValue(value_list[string], hashtable)
+        value_key_list.append(value_key)
+    return value_key_list
+
+
 def getKeyFromValue(value, hashtable):
     for key, potential_value in hashtable.items():
         if value == potential_value:
             return key
     raise KeyError
-
 
 def populateHashtable(iomtdata):
     '''
@@ -61,37 +68,64 @@ def conversion(generated, name_hashtable, cpu_hashtable, sensor_hashtable, conne
     '''
     Converts generated.json file into numpy array for input to ML algorithms
     '''
-    device_array = [0,0,0,0,0,0,0,0]
+    to_convert = []
 
 #Opening generated file:
     with open(generated, 'r') as f:
         convert_dict = json.load(f)
 
     for device in range(0, len(convert_dict)):
+        device_array = [0,0,0,0,0,0,0,-1]
         current_device = 'Device ' + str(device)
+        current_value_list = list(convert_dict[current_device].values())
 
+    #Deals with integer values of generated.json
+        device_array[1] = [convert_dict[current_device]["Width"]]
+        device_array[2] = [convert_dict[current_device]["Length"]]
+        device_array[3] = [convert_dict[current_device]["Height"]]
+        device_array[4] = [convert_dict[current_device]["Weight"]]
+
+    #Deals with string values of generated.json
         try:
-            device_array[0] = getKeyFromValue(convert_dict[current_device]["CPU"][0], cpu_hashtable)
+            device_array[0] = formatStringList(current_value_list[0], cpu_hashtable)
+            device_array[5] = formatStringList(current_value_list[5], sensor_hashtable)
+            device_array[6] = formatStringList(current_value_list[6], connect_hashtable)
+
+            if len(current_value_list) > 7:
+                name_list = []
+                name_list.append(current_value_list[7])
+                device_array[7] = formatStringList(name_list, name_hashtable)
+
+            to_convert.append(device_array)
         except KeyError:
-            print("One or more key(s) not in hashtable.")
+            print("Key not found in dictionary!")
 
-    # for device in range(0, len(convert_dict)):
-    #     current_device = 'Device ' + str(device)
-    #     device_array.append([
-    #     [convert_dict[current_device]["Width"]],
-    #     [convert_dict[current_device]["Length"]],
-    #     [convert_dict[current_device]["Height"]],
-    #     [convert_dict[current_device]["Weight"]],
-    #     ])
-
-    #Convert to numpy array
-    numpy_device_array = np.asarray(device_array, dtype=np.float32)
-
-    print(numpy_device_array)
-    print(cpu_hashtable)
+    converted_array = np.asarray(to_convert)
+    return converted_array
 
 
+def main():
+
+    nametable, cputable, sensortable, connectable = populateHashtable(iomtDataFile)
+    output = conversion(generatedDataFile, nametable, cputable, sensortable, connectable)
+
+    #Un-comment to print hashtables for reference!
+    # for item in list(nametable.items()):
+    #     print(item)
+    # print("\n")
+    # for item in list(cputable.items()):
+    #     print(item)
+    # print("\n")
+    # for item in list(sensortable.items()):
+    #     print(item)
+    # print("\n")
+    # for item in list(connectable.items()):
+    #     print(item)
+    # print("\n")
+
+    print("Conversion complete.")
+    return output
 
 
-nametable, cputable, sensortable, connectable = populateHashtable(iomtDataFile)
-conversion(generatedDataFile, nametable, cputable, sensortable, connectable)
+
+main()
